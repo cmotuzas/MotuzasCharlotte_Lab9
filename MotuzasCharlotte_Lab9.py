8bb717eed763b1bd35058101fc03d386f19a485a
cmotuzas <cmotuzas@uwo.ca>
2024-11-13 15:31:32 -0500

    Update MotuzasCharlotte_Lab9.py

--- MotuzasCharlotte_Lab9.py
+++ MotuzasCharlotte_Lab9.py
@@ -5,87 +5,71 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def traffic_solution(N,L,v_max,nstep,rho_max):
+    #* Select numerical parameters (time step, grid spacing, etc.).
+    h = L/N       # Grid spacing for periodic boundary conditions
+    tau = h/v_max
+    coeff = tau/(2*h)          # Coefficient used by all schemes
 
-#* Select numerical parameters (time step, grid spacing, etc.).
-N = 600
-L = 1200.      # System size (meters)
-h = 2       # Grid spacing for periodic boundary conditions
-v_max = 25.    # Maximum car speed (m/s)
-tau = h/v_max
-nstep = 1500
-coeff = tau/(2*h)          # Coefficient used by all schemes
+    #* Set initial and boundary conditions
+    rho_max = 1.0                   # Maximum density
+    Flow = np.empty(N)
+    # Initial condition is a square pulse from x = -L/4 to x = 0
+    rho = np.zeros(N)
+    for i in range(int(N/4),int(N/2)) :
+        rho[i] = rho_max     # Max density in the square pulse
 
-#* Set initial and boundary conditions
-rho_max = 1.0                   # Maximum density
-Flow_max = 0.25*rho_max*v_max   # Maximum Flow
-Flow = np.empty(N)
-cp = np.empty(N);  cm = np.empty(N)
-# Initial condition is a square pulse from x = -L/4 to x = 0
-rho = np.zeros(N)
-for i in range(int(N/4),int(N/2)) :
-    rho[i] = rho_max     # Max density in the square pulse
+    rho[int(N/2)] = rho_max/2   # Try running without this line
 
-rho[int(N/2)] = rho_max/2   # Try running without this line
+    # Use periodic boundary conditions
+    ip = np.arange(N) + 1  
+    ip[N-1] = 0          # ip = i+1 with periodic b.c.
+    im = np.arange(N) - 1  
+    im[0] = N-1          # im = i-1 with periodic b.c.
 
-# Use periodic boundary conditions
-ip = np.arange(N) + 1  
-ip[N-1] = 0          # ip = i+1 with periodic b.c.
-im = np.arange(N) - 1  
-im[0] = N-1          # im = i-1 with periodic b.c.
-
-#* Initialize plotting variables.
-iplot = 1
-xplot = (np.arange(N)-1/2.)*h - L/2.    # Record x scale for plot
-rplot = np.empty((N,nstep+1))
-tplot = np.empty(nstep+1)
-rplot[:,0] = np.copy(rho)   # Record the initial state
-tplot[0] = 0                # Record the initial time (t=0)
+    #* Initialize plotting variables.
+    iplot = 1
+    xplot = (np.arange(N)-1/2.)*h - L/2.    # Record x scale for plot
+    rplot = np.empty((N,nstep+1))
+    tplot = np.empty(nstep+1)
+    rplot[:,0] = np.copy(rho)   # Record the initial state
+    tplot[0] = 0                # Record the initial time (t=0)
 
 
-#* Loop over desired number of steps.
-for istep in range(nstep) :
+    #* Loop over desired number of steps.
+    for istep in range(nstep) :
 
-    #* Compute the flow = (Density)*(Velocity)
-    Flow[:] = rho[:] * (v_max*(1 - rho[:]/rho_max))
-  
-    #* Compute new values of density using  
-    #  FTCS, Lax or Lax-Wendroff method.
+        #* Compute the flow = (Density)*(Velocity)
+        Flow[:] = rho[:] * (v_max*(1 - rho[:]/rho_max))
+    
+        #* Compute new values of density using Lax method 
+        rho[:] = .5*( rho[ip] + rho[im] ) - coeff*( Flow[ip] - Flow[im] )
 
-    rho[:] = .5*( rho[ip] + rho[im] ) - coeff*( Flow[ip] - Flow[im] )
+        #* Record density for plotting.
+        rplot[:,iplot] = np.copy(rho)
+        tplot[iplot] = tau*(istep+1)
+        iplot += 1
+    return xplot, rplot, tplot, iplot
 
-    #if method == 1 :      ### FTCS method ###
-    #    rho[:] = rho[:] - coeff*( Flow[ip] - Flow[im] )
-    #elif method == 2 :    ### Lax method ###
-    #    rho[:] = .5*( rho[ip] + rho[im] ) - coeff*( Flow[ip] - Flow[im] )
-    #else :                ### Lax-Wendroff method ###
-    #    cp[:] = v_max*(1 - (rho[ip]+rho[:])/rho_max);
-    #    cm[:] = v_max*(1 - (rho[:]+rho[im])/rho_max);
-    #    rho[:] = rho[:] - coeff*( Flow[ip] - Flow[im] ) + coefflw*(
-    #        cp[:]*(Flow[ip]-Flow[:]) - cm[:]*(Flow[:]-Flow[im]) )
-
-    #* Record density for plotting.
-    rplot[:,iplot] = np.copy(rho)
-    tplot[iplot] = tau*(istep+1)
-    iplot += 1
-
+xplot, rplot, tplot, iplot = traffic_solution(600,1200,25,1500,1)
 
 #* Graph density versus position and time as wire-mesh plot
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D
 
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-Tp, Xp = np.meshgrid(tplot[0:iplot], xplot)
-ax.plot_surface(Tp, Xp, rplot[:,0:iplot], rstride=1, cstride=1, cmap=cm.gray)
-ax.view_init(elev=30., azim=10.)
-ax.set_xlabel('t')
-ax.set_ylabel('x')
-ax.set_zlabel('rho')
-ax.set_title('Density versus position and time')
-plt.show()
+#fig = plt.figure()
+#ax = fig.add_subplot(projection='3d')
+#Tp, Xp = np.meshgrid(tplot[0:iplot], xplot)
+#ax.plot_surface(Tp, Xp, rplot[:,0:iplot], rstride=1, cstride=1, cmap=cm.gray)
+#ax.view_init(elev=30., azim=10.)
+#ax.set_xlabel('t')
+#ax.set_ylabel('x')
+#ax.set_zlabel('rho')
+#ax.set_title('Density versus position and time')
+#plt.show()
 
 #* Graph contours of density versus position and time.
-levels = np.linspace(0., 1., num=11) 
+levels = np.linspace(0., 1., num=15) 
 ct = plt.contour(xplot, tplot, np.flipud(np.rot90(rplot)), levels) 
 plt.clabel(ct, fmt='%1.2f') 
 plt.xlabel('x')
